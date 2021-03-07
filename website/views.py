5290645bc50b8faeca2c406355fd08fb2dbdc192
ttxrps <76846365+ttxrps@users.noreply.github.com>
2021-03-07 21:13:24 +0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -3,8 +3,7 @@ from flask_login import login_required, current_user
 from urllib.parse import quote
 from urllib.request import urlopen
 from . import db
-import json
-import requests
+from .function import fetchAPOD
 
 URL_APOD = "https://api.nasa.gov/planetary/apod{0}"
 api_key = 'PIQgwKgT5WieoxPWMksJNr1GtdtIktdVc01dc6Jr'
@@ -23,22 +22,7 @@ def home():
     return render_template("home.html", user=current_user, APOD=APOD)
 
 
-def fetchAPOD():
-    URL_APOD = "https://api.nasa.gov/planetary/apod"
-    api_key = 'PIQgwKgT5WieoxPWMksJNr1GtdtIktdVc01dc6Jr'
-    date = '2021-02-13'
-    params = {
-        'api_key': api_key,
-        'date': date,
-        'hd': 'True'
-    }
-    response = requests.get(URL_APOD, params=params).json()
-    APOD = {'title': response['title'],
-                    'explanation': response['explanation'],
-                    'daily': response['date'],
-                    'hdurl': response['hdurl']
-                    }
-
-    return APOD
-
-
+@views.route('/team', methods=['GET', 'POST'])
+@login_required
+def team():
+    return render_template('team.html',user=current_user)
