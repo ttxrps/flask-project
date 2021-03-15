0b8a876ef694a9873e32203e8206a007aaa278b0
ttxrps <76846365+ttxrps@users.noreply.github.com>
2021-03-16 02:09:08 +0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 # create function create app for main.py
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'ghdfjdjlfkl jgkfjlf'
+    app.config['SECRET_KEY'] = 'dsfjksdkjlflks'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
