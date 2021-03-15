4920cd6dda23bc5183a5d7d9109c8eb5db40cb99
ttxrps <76846365+ttxrps@users.noreply.github.com>
2021-03-16 02:07:27 +0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from urllib.parse import quote
-from urllib.request import urlopen
-from . import db
-from .function import fetchAPOD
+from website.models import User
+from .function import fetchAPOD,fetchAsteroidNeowsFeed,fetchInSightMars,get_image
+
+
 
 # create Blueprint name => views
 views = Blueprint('views', __name__)
@@ -16,8 +16,25 @@ def home():
     APOD = fetchAPOD()
     return render_template("home.html", user=current_user, APOD=APOD)
 
-
 @views.route('/team', methods=['GET', 'POST'])
 @login_required
 def team():
-    return render_template('team.html',user=current_user)
+    return render_template('team.html',user=current_user,teams = User.query.all())
+    
+@views.route('/neo', methods=['GET', 'POST'])
+@login_required
+def neo():
+    myFeed = fetchAsteroidNeowsFeed()
+    return render_template('neo.html', user=current_user, myFeed=myFeed)
+
+@views.route('/mars', methods=['GET', 'POST'])
+@login_required
+def Mars():
+    mars = fetchInSightMars()
+    return render_template('mars.html', user=current_user,mars=mars)
+
+@views.route('/earth', methods=['GET', 'POST'])
+@login_required
+def earth():
+    mylist = get_image()
+    return render_template('earth.html',user=current_user,list = mylist)
